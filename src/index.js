8d6223f2c936e54a0f221e14165e7bab51fe3b2d
ThinLiquid <thinliquid@proton.me>
2023-07-13 02:14:33 +0000

    refractor: remove useless logs

--- src/index.js
+++ src/index.js
@@ -102,21 +102,13 @@ if (isNaN(port)) port = 8080;
 server.on('listening', () => {
 	const address = server.address();
 
-	console.log('Listening on:');
-	console.log(`\thttp://localhost:${address.port}`);
-	console.log(`\thttp://${hostname()}:${address.port}`);
-	console.log(
-		`\thttp://${
-      address.family === 'IPv6' ? `[${address.address}]` : address.address
-    }:${address.port}`
-	);
+	console.log(`Listening on: http://localhost:${address.port}`);
 });
 
 process.on('SIGINT', shutdown);
 process.on('SIGTERM', shutdown);
 
 function shutdown() {
-	console.log('SIGTERM signal received: closing HTTP server');
 	server.close();
 	bare.close();
 	process.exit(0);
