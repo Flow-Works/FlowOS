8e25ce007845ed38b4e900e5f47b585122fa001f
ThinLiquid <thinliquid@proton.me>
2023-07-13 15:50:35 +0000

    perf: change cache maxage

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ function shouldCompress (req, res) {
 }
 
 app.use((req, res, next) => {
-	res.setHeader('Cache-Control', 'public, s-maxage=3600');
+	res.setHeader('Cache-Control', 'public, s-maxage=2592000');
 	next();
 });
 
