5be3c883cd4f77a1b9c5bbc19d047f5435ddc7d7
ThinLiquid <thinliquid@proton.me>
2023-07-13 11:44:10 +0000

    refactor: switch to sha256

--- src/password.js
+++ src/password.js
@@ -8,15 +8,7 @@ const key = Buffer.from(String(process.env.KEY), 'hex');
 const iv = Buffer.from(String(process.env.IV), 'hex');
 
 function encrypt(plainText) {
-	var mykey = crypto.createCipheriv('aes-128-gcm', key, iv);
-	var mystr = mykey.update(plainText, 'utf8', 'hex');
-	mystr += mykey.final('hex');
-	return mystr;
-}
-
-function decrypt(cipherText) {
-	var mykey = crypto.createDecipheriv('aes-128-gcm', key, iv);
-	var mystr = mykey.update(cipherText, 'hex', 'utf8');
+	var mystr = crypto.createHash('sha256').update(plainText).update(makeHash(process.env.SALT)).digest('hex');
 	return mystr;
 }
 
@@ -25,19 +17,23 @@ const cacheMiddleware = (req, res, next) => {
 	const cachedData = cache.get(cache_key);
   
 	if (cachedData) {
-	  res.send(cachedData);
-	  return;
+	  	res.send(cachedData);
+	  	return;
 	}
   
 	res.sendResponse = res.send;
 	res.send = (body) => {
-	  const durationInMilliseconds = 5 * 60 * 1000;
-	  cache.put(cache_key, body, durationInMilliseconds);
-	  res.sendResponse(body);
+	  	const durationInMilliseconds = 5 * 60 * 1000;
+	  	cache.put(cache_key, body, durationInMilliseconds);
+	  	res.sendResponse(body);
 	};
   
 	next();
-  };
+};
+
+function makeHash(val) {
+    return crypto.createHash('sha256').update(val, 'utf-8').digest();
+}
   
 
 router.get('/encrypt', cacheMiddleware, (req, res) => {
@@ -47,9 +43,9 @@ router.get('/encrypt', cacheMiddleware, (req, res) => {
 });
 
 router.get('/verify', cacheMiddleware, (req, res) => {
-	const dec = decrypt(req.query.aes);
+	const enc = encrypt(req.query.input);
 	res.setHeader('Content-Type', 'text/plain');
-	res.send(dec == req.query.input);
+	res.send(enc == req.query.hash);
 });
 
 export default router;
