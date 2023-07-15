f2b1158a345ef138ba76821c91e9bd5d19cc0963
ThinLiquid <thinliquid@proton.me>
2023-07-15 02:46:45 +0000

    perf: remove uv.bundle.js

--- FlowOS/public/uv/uv.config.js
+++ FlowOS/public/uv/uv.config.js
@@ -1,10 +1,16 @@
 // This file overwrites the stock UV config.js
 
+const xor = {
+	encode: (str, key=2) => {
+		return encodeURIComponent(str.split('').map(e=>e.charCodeAt()).map((e,i)=>i%key?String.fromCharCode(e ^ 2) : String.fromCharCode(e)).join(''))
+	}
+};
+
 self.__uv$config = {
 	prefix: '/uv/service/',
 	bare: '/bare/',
-	encodeUrl: Ultraviolet.codec.xor.encode,
-	decodeUrl: Ultraviolet.codec.xor.decode,
+	encodeUrl: xor.encode,
+	decodeUrl: null,
 	handler: '/uv/uv.handler.js',
 	client: '/uv/uv.client.js',
 	bundle: '/uv/uv.bundle.js',
