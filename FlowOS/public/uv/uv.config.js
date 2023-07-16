acd1a43f8403c7467a82c442cefbb9b738198e7b
ThinLiquid <thinliquid@proton.me>
2023-07-16 05:37:18 +0000

    :zap: perf: remove uv.bundle.js

--- FlowOS/public/uv/uv.config.js
+++ FlowOS/public/uv/uv.config.js
@@ -1,10 +1,21 @@
 // This file overwrites the stock UV config.js
 
+const plain = {
+	encode: (str) => {
+		if (!str) return str;
+		return encodeURIComponent(str);
+	},
+	decode: (str) => {
+		if (!str) return str;
+		return decodeURIComponent(str);
+	},
+};
+
 self.__uv$config = {
 	prefix: '/uv/service/',
 	bare: '/bare/',
-	encodeUrl: Ultraviolet.codec.plain.encode,
-	decodeUrl: Ultraviolet.codec.plain.decode,
+	encodeUrl: plain.encode,
+	decodeUrl: plain.decode,
 	handler: '/uv/uv.handler.js',
 	client: '/uv/uv.client.js',
 	bundle: '/uv/uv.bundle.js',
