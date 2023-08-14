188b87714d8da6fdbc091eccc0f70c565314784e
Systemless <95631896+RisingGlitch@users.noreply.github.com>
2023-08-14 02:18:24 +0000

    Add Help Cmd

---
+++ public/builtin/apps/scripts/terminal/commands/help.js
@@ -0,0 +1,14 @@
+export const metadata = {
+    cmd: 'help',
+    description: 'View what different commands do.'
+};
+
+
+export const exec = async (fs, term, usr, dir, args) => {
+    term.writeln("Fetching command...");
+    var cmd ="";
+    await import(window.location.origin + "/builtin/apps/scripts/terminal/commands/" + args[1] + ".js").then (async(command)=> {
+        term.writeln(args[1]+": "+ await command.metadata.description);
+    });
+    return;
+};
